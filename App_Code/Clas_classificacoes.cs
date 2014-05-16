0817ce373b04927ccd2a0ba3a956882b78e008e5
Coltinne <nicholas.coltinne@gmail.com>
2014-05-15 22:54:11 -0300

    0.21

---
+++ App_Code/Clas_classificacoes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Clas_classificacoes
+/// </summary>
+public class Clas_classificacoes
+{
+	
+}
